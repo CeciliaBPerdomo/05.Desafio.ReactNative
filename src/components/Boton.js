c05ddb11b543f848c7e3233d4b3b0b308b9b914d
Cecilia <cecilia.perdomo@gmail.com>
2024-01-31 16:13:10 -0300

    Personalizado el boton

--- src/components/Boton.js
+++ src/components/Boton.js
@@ -1,13 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 
 const Boton = () => {
   return (
-    <View>
-      <Text>Boton</Text>
+    <View style={styles.container}>
+      <Pressable style={styles.boton}>
+        <Text style={styles.texto}>Guardar libro</Text>
+      </Pressable>
     </View>
   )
 }
 
 export default Boton
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+    container: {
+        backgroundColor: "#FDFFAB",
+        padding: 5, 
+    },
+
+    boton: {
+        backgroundColor: "#FDFFAB",
+        width: "50%",
+        height: 40,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        marginHorizontal: 10,
+        marginVertical: 5,
+        borderWidth: 2,
+        borderColor: "#FFCF81",
+        fontSize: 16,
+        borderRadius: 10,
+        alignSelf:"flex-end",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    texto: {
+        fontSize: 16, 
+        color: "#872FF5"
+    }
+    
+})
